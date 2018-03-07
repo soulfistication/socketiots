995113a90d5682c02a66d1adba78c2ce89709251
Ivan Almada <ivan.almada@green-light.com>
2018-03-07 11:44:34 +0100

    Logs a message also when the user disconnects.

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ app.get('/index.html', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
     console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    })
 });
 
 http.listen(3000, function () {
