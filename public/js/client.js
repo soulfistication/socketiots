eb7d64e29e78839b861d252bf7bf10286590dbee
Ivan Almada <ivan.almada@green-light.com>
2018-03-09 15:35:03 +0100

    Transfered the initial log and button handler to the jquery on document ready.

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,15 @@
 /// <reference path="../../typings/globals/jquery/index.d.ts" />
 /// <reference path="../../typings/globals/socket.io/index.d.ts" />
 
-console.log('client connected');
+$(function() {
+    console.log('client connected');
 
-var socket = io();
+    var socket = io();
 
+    $('form').submit(function() {
+        console.log('button clicked!');
+        return false;
+    });
+
+    $('body').css('background', 'red');
+});
