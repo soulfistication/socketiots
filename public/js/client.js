e45d53830265366c0e2943ec691a290e52ed40c3
Ivan Almada <ivan.almada@green-light.com>
2018-03-09 15:33:42 +0100

    Added typing definitions for socket.io.

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,7 @@
 /// <reference path="../../typings/globals/jquery/index.d.ts" />
+/// <reference path="../../typings/globals/socket.io/index.d.ts" />
 
 console.log('client connected');
 
-$('body').css('background', 'red')
+var socket = io();
+
